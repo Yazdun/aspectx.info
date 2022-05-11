efabb490a1c445cf4a1ad357a65f2e91f45bd686
Yazdun <yazduntube@gmail.com>
2022-05-11 18:09:19 +0430

    menu : updated hamburger color based on theme

--- components/menu/index.jsx
+++ components/menu/index.jsx
@@ -1,9 +1,17 @@
 import { Squash as Hamburger } from 'hamburger-react'
+import { useTheme } from 'next-themes'
 import { useState } from 'react'
 import css from './styles.module.css'
 
 export const Menu = () => {
   const [isOpen, setOpen] = useState(false)
+  const { theme } = useTheme()
+
+  const isDark =
+    theme === 'dark' ||
+    (theme === 'system' &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches)
+
   return (
     <>
       <div className={css.cta}>
@@ -12,6 +20,7 @@ export const Menu = () => {
           size={26}
           toggled={isOpen}
           toggle={setOpen}
+          color={isDark ? 'white' : 'black'}
         />
       </div>
     </>
