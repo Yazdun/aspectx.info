300fa55a7b59b70423d1b015e394d899920258c7
Yazdun <yazduntube@gmail.com>
2022-05-22 10:24:07 +0430

    storefronts : completed

--- components/storefront/index.jsx
+++ components/storefront/index.jsx
@@ -1,12 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 import css from './styles.module.css'
 
 export const Storefront = ({ store }) => {
-  const { domain, name, slug, icon, games } = store
+  const { domain, name, icon } = store
   return (
-    <div className={css.card}>
-      <div className={css.icon}>{icon}</div>
-      <h2>{name}</h2>
-    </div>
+    <Link href={`https://${domain}`}>
+      <a className={css.card} target="_blank">
+        <div className={css.icon}>{icon}</div>
+        <h2 className={css.name}>{name}</h2>
+      </a>
+    </Link>
   )
 }
--- pages/stores/index.jsx
+++ pages/stores/index.jsx
@@ -1,11 +1,19 @@
 import css from './index.module.css'
 import { Container, Layout, Storefront } from 'components'
+import { Storefronts } from 'utils'
 
 export default function Stores() {
   return (
     <Layout title="AspectX | Stores">
-      <Container sx={css.grid}>
-        <h1>Storefronts</h1>
+      <Container gap>
+        <h1 style={{ marginBottom: '5rem', textAlign: 'center' }}>
+          Top Videogame&apos;s Storefronts
+        </h1>
+        <div className={css.grid}>
+          {Storefronts.map(store => {
+            return <Storefront key={store.id} store={store} />
+          })}
+        </div>
       </Container>
     </Layout>
   )
