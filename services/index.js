64f439fad5956f1ff177aa090f477a333d9cf465
Yazdun <yazduntube@gmail.com>
2022-05-25 11:56:11 +0430

    services : updated API KEY

--- services/index.js
+++ services/index.js
@@ -11,32 +11,32 @@ export const RAWG_TRENDING = `${ENDPOINT}/games?dates=${today.getFullYear()}-${(
 ).slice(-2)}-10,${today.getFullYear() + 1}-${(
   '0' +
   (today.getMonth() + 1)
-).slice(-2)}-10&ordering=-added&key=${process.env.API_KEY}`
+).slice(-2)}-10&ordering=-added&key=${process.env.NEXT_PUBLIC_API_KEY}`
 
 // get game with slug
 export const RAWG_GAME_SLUG = slug =>
-  `${ENDPOINT}/games/${slug}?&key=${process.env.API_KEY}`
+  `${ENDPOINT}/games/${slug}?&key=${process.env.NEXT_PUBLIC_API_KEY}`
 
 // get creatores
-export const RAWG_CREATORS = `${ENDPOINT}/creators?page_size=20&key=${process.env.API_KEY}`
+export const RAWG_CREATORS = `${ENDPOINT}/creators?page_size=20&key=${process.env.NEXT_PUBLIC_API_KEY}`
 
 // find creator
 export const RAWG_FIND_CREATOR = id =>
-  `${ENDPOINT}/creators/${id}?key=${process.env.API_KEY}`
+  `${ENDPOINT}/creators/${id}?key=${process.env.NEXT_PUBLIC_API_KEY}`
 
 // get creator's game
 export const RAWG_FIND_CREATOR_GAMES = id =>
-  `${ENDPOINT}/games?key=${process.env.API_KEY}&creators=${id}`
+  `${ENDPOINT}/games?key=${process.env.NEXT_PUBLIC_API_KEY}&creators=${id}`
 
 // get all games
-export const RAWG_ALL_GAMES = `${ENDPOINT}/games?key=${process.env.API_KEY}&ordering=-added&page_size=12`
+export const RAWG_ALL_GAMES = `${ENDPOINT}/games?key=${process.env.NEXT_PUBLIC_API_KEY}&ordering=-added&page_size=12`
 
 // get game screenshots
 export const RAWG_GAME_SLUG_SCREENSHOTS = slug =>
-  `${ENDPOINT}/games/${slug}/screenshots?&key=${process.env.API_KEY}`
+  `${ENDPOINT}/games/${slug}/screenshots?&key=${process.env.NEXT_PUBLIC_API_KEY}`
 
 // get high rated games
-export const RAWG_HIGH_RATED_GAMES = `${ENDPOINT}/games?metacritic=90,100&page_size=38&ordering=-added&key=${process.env.API_KEY}`
+export const RAWG_HIGH_RATED_GAMES = `${ENDPOINT}/games?metacritic=90,100&page_size=38&ordering=-added&key=${process.env.NEXT_PUBLIC_API_KEY}`
 
 // search games
 export const RAWG_SEARCH = searchTerm =>
